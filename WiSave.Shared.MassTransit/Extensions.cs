5e06794943899133eaba0e59178ec81a93d07b39
Qbash <jacobchwastek@gmail.com>
2025-05-17 01:39:26 +0200

    Point Masstransit.Abstraction directly

--- WiSave.Shared.MassTransit/Extensions.cs
+++ WiSave.Shared.MassTransit/Extensions.cs
@@ -21,41 +21,42 @@ public static class Extensions
         services.AddMassTransit<TBus>(x =>
         {
             x.SetEndpointNameFormatter(new KebabCaseEndpointNameFormatter(includeNamespace: false));
-            
+
             x.AddConsumers(consumerTypes);
-            
+
             x.AddDelayedMessageScheduler();
 
             configureMassTransit?.Invoke(x);
             
-            x.AddConfigureEndpointsCallback((context,name,cfg) =>
+            x.AddConfigureEndpointsCallback((context, _, cfg) =>
             {
                 cfg.UseDelayedRedelivery(r => r.Intervals(TimeSpan.FromMinutes(5), TimeSpan.FromMinutes(15), TimeSpan.FromMinutes(30)));
                 cfg.UseMessageRetry(r => r.Immediate(5));
                 cfg.UseInMemoryOutbox(context);
             });
-            
+
             x.UsingRabbitMq((context, cfg) =>
             {
+                cfg.UseJsonSerializer();
+                
                 cfg.Host(rabbitMqConfiguration.Host, rabbitMqConfiguration.Vhost, h =>
                 {
                     h.Username(rabbitMqConfiguration.Username);
                     h.Password(rabbitMqConfiguration.Password);
                 });
-                
+
                 cfg.ConfigureJsonSerializerOptions(options =>
                 {
                     options.Converters.Add(new DateOnlyJsonConverter());
                     return options;
                 });
-                
+
                 configureBroker?.Invoke(cfg, context);
 
                 cfg.ConfigureEndpoints(context);
             });
         });
-        
+
         return services;
     }
-    
 }
